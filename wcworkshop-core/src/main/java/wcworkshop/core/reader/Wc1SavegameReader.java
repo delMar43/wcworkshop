f2aa174201c8a33747110b5d193f0c523f6cef0c
delMar43 <martingu1979@gmail.com>
2013-11-24 00:53:58 +0100

    fixed merge

--- wcworkshop-core/src/main/java/wcworkshop/core/reader/Wc1SavegameReader.java
+++ wcworkshop-core/src/main/java/wcworkshop/core/reader/Wc1SavegameReader.java
@@ -5,74 +5,7 @@ import java.util.Arrays;
 import wcworkshop.core.data.Wc1Savegame;
 
 public class Wc1SavegameReader {
-<<<<<<< HEAD
   private static final Wc1SavegameReader instance = new Wc1SavegameReader();
-=======
-
-  private static final Wc1SavegameReader instance = new Wc1SavegameReader();
-
-  private static final short OFFSET_NAME = 0; //size=16
-  private static final byte OFFSET_OCCUPIED = 17;//0=empty, 1=used
-
-  private static final short OFFSET_PILOT_NAME = 0; //size=13 (=38*playerIndex+18)
-  private static final short OFFSET_PILOT_CALLSIGN = 14; //size=13
-  private static final short OFFSET_PILOT_RANK = 16;//short
-  private static final short OFFSET_PILOT_SORTIES = 18;//short
-  private static final short OFFSET_PILOT_KILLS = 20;//short
-
-  private static final short OFFSET_PILOT1_NAME = 56;//short
-  private static final short OFFSET_PILOT1_CALLSIGN = 70;//short
-  private static final short OFFSET_PILOT1_RANK = 86;//short 
-  private static final short OFFSET_PILOT1_SORTIES = 88;//short
-  private static final short OFFSET_PILOT1_KILLS = 90;//short
-
-  private static final short OFFSET_PILOT2_RANK = 124;//short
-
-  private static final short OFFSET_PILOT3_RANK = 162;
-  private static final short OFFSET_PILOT3_SORTIES = 164;
-  private static final short OFFSET_PILOT3_KILLS = 166;
-
-  private static final short OFFSET_PILOT4_RANK = 200;
-
-  private static final short OFFSET_PILOT5_RANK = 238;
-
-  private static final short OFFSET_PILOT6_RANK = 276;
-
-  private static final short OFFSET_PILOT7_RANK = 314;
-
-  private static final short OFFSET_PLAYER_RANK = 352;
-  private static final short OFFSET_PLAYER_SORTIES = 354;
-  private static final short OFFSET_PLAYER_KILLS = 356;
-
-  private static final short OFFSET_BRONZE = 364;//byte
-  private static final short OFFSET_SILVER = 365;//byte
-  private static final short OFFSET_GOLD = 366;//byte
-  private static final short OFFSET_GOLDEN_SUN = 367;//byte
-
-  private static final short OFFSET_PILOT_STATUS = 392;//short (=pilotIndex*2+392);
-  private static final short OFFSET_PILOT1_STATUS = 394;//short
-  private static final short OFFSET_PILOT2_STATUS = 396;//short
-  private static final short OFFSET_PILOT3_STATUS = 398;//short
-  private static final short OFFSET_PILOT4_STATUS = 400;//short
-  private static final short OFFSET_PILOT5_STATUS = 402;//short
-  private static final short OFFSET_PILOT6_STATUS = 404;//short
-  private static final short OFFSET_PILOT7_STATUS = 406;//short
-
-  private static final short OFFSET_DAY = 412;
-  private static final short OFFSET_PROMOTION = 420; //short
-
-  private static final short OFFSET_MISSION = 381;//byte
-  private static final short OFFSET_SERIES = 382;//byte
-
-  private static final short OFFSET_BHURAK = 408; //0A = KIA, 01 = not seen, 29 = fled
-  private static final short OFFSET_DAKHATH = 409; //0A = KIA, 01 = not seen
-  private static final short OFFSET_KHAJJA = 410;//0A = KIA, 01 = not seen
-  private static final short OFFSET_BAKTOSH = 411;//0A = KIA, 01 = not seen
-
-  private static final short OFFSET_VICTORY = 424; //short
-  private static final short OFFSET_CAMPAIGN = 426;//byte 00=Vega, 01=SM
-
->>>>>>> branch 'master' of https://github.com/delMar43/wcworkshop.git
   private ReaderHelper readerHelper = ReaderHelper.getInstance();
 
   public Wc1Savegame[] read(String path) {
@@ -85,11 +18,7 @@ public class Wc1SavegameReader {
       int offset = index * 828;
       byte[] savegameBlock = Arrays.copyOfRange(file, offset, offset + blocksize);
       Wc1Savegame savegame;
-<<<<<<< HEAD
       if (savegameBlock[Wc1SavegameOffsets.OFFSET_OCCUPIED] == (byte) 1) {
-=======
-      if (savegameBlock[OFFSET_OCCUPIED] == (byte) 1) {
->>>>>>> branch 'master' of https://github.com/delMar43/wcworkshop.git
         savegame = extractSavegame(savegameBlock);
       } else {
         savegame = Wc1Savegame.EMPTY;
@@ -103,8 +32,7 @@ public class Wc1SavegameReader {
   private Wc1Savegame extractSavegame(byte[] savegameBlock) {
     Wc1Savegame savegame = new Wc1Savegame();
 
-<<<<<<< HEAD
-    savegame.setName(readerHelper.getString(savegameBlock, Wc1SavegameOffsets.NAME_OFFSET, Wc1SavegameOffsets.NAME_LENGTH));
+    savegame.setName(readerHelper.getString(savegameBlock, Wc1SavegameOffsets.NAME_OFFSET, 14));
 
     for (int pilotIndex = 0; pilotIndex < 8; ++pilotIndex) {
       int pilotOffset = 38 * pilotIndex + 18;
@@ -130,34 +58,6 @@ public class Wc1SavegameReader {
     byte series = savegameBlock[Wc1SavegameOffsets.OFFSET_SERIES];
     byte campaign = savegameBlock[Wc1SavegameOffsets.OFFSET_CAMPAIGN];
     short victoryPoints = readerHelper.getShort(savegameBlock, Wc1SavegameOffsets.OFFSET_VICTORY);
-=======
-    savegame.setName(readerHelper.getString(savegameBlock, OFFSET_NAME, 14));
-
-    for (int pilotIndex = 0; pilotIndex < 8; ++pilotIndex) {
-      int pilotOffset = 38 * pilotIndex + 18;
-
-      String name = readerHelper.getString(savegameBlock, pilotOffset + OFFSET_PILOT_NAME, 13);
-      String callsign = readerHelper.getString(savegameBlock, pilotOffset + OFFSET_PILOT_CALLSIGN, 13);
-      short rank = readerHelper.getShort(savegameBlock, pilotOffset + OFFSET_PILOT_RANK);
-      short sorties = readerHelper.getShort(savegameBlock, pilotOffset + OFFSET_PILOT_SORTIES);
-      short kills = readerHelper.getShort(savegameBlock, pilotOffset + OFFSET_PILOT_KILLS);
-
-      int pilotStatusOffset = pilotIndex * 2 + 392;
-      byte status = savegameBlock[pilotStatusOffset + OFFSET_PILOT_STATUS];
-      short day = readerHelper.getShort(savegameBlock, OFFSET_DAY);
-      short promotion = readerHelper.getShort(savegameBlock, OFFSET_PROMOTION);
-    }
-
-    byte bronzeStars = savegameBlock[OFFSET_BRONZE];
-    byte silverStars = savegameBlock[OFFSET_SILVER];
-    byte goldStars = savegameBlock[OFFSET_GOLD];
-    byte goldenSun = savegameBlock[OFFSET_GOLDEN_SUN];
-
-    byte mission = savegameBlock[OFFSET_MISSION];
-    byte series = savegameBlock[OFFSET_SERIES];
-    byte campaign = savegameBlock[OFFSET_CAMPAIGN];
-    short victoryPoints = readerHelper.getShort(savegameBlock, OFFSET_VICTORY);
->>>>>>> branch 'master' of https://github.com/delMar43/wcworkshop.git
 
     byte bhurak = savegameBlock[408];
     byte dakhath = savegameBlock[409];
--- wcworkshop-web/src/main/java/wcworkshop/web/controller/SavegamePostController.java
+++ wcworkshop-web/src/main/java/wcworkshop/web/controller/SavegamePostController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-<<<<<<< HEAD
 import wcworkshop.core.writer.Wc1SavegameWriter;
 import wcworkshop.web.command.SavegameCommand;
 
@@ -18,15 +17,6 @@ public class SavegamePostController {
   public String saveGame(@ModelAttribute SavegameCommand savegameCommand) {
 
     writer.write(savegameCommand.getSavegames(), "D:/Users/martin/Dropbox/dev/wcworkshop/gamedat/wc1/savegame.wld");
-=======
-import wcworkshop.web.command.SavegameCommand;
-
-@Controller
-public class SavegamePostController {
-
-  @RequestMapping(value = "/savegame", method = RequestMethod.POST)
-  public String saveGame(@ModelAttribute SavegameCommand savegameCommand) {
->>>>>>> branch 'master' of https://github.com/delMar43/wcworkshop.git
 
     return "forward:savegameEditor.html";
   }
