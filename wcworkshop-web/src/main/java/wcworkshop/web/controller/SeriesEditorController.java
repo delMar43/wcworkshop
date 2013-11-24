23f283fbe145bec16d8b896c3e7b5e5876632278
delMar43 <martingu1979@gmail.com>
2013-11-24 21:10:23 +0100

    linking between series

--- wcworkshop-web/src/main/java/wcworkshop/web/controller/SeriesEditorController.java
+++ wcworkshop-web/src/main/java/wcworkshop/web/controller/SeriesEditorController.java
@@ -1,17 +1,29 @@
 package wcworkshop.web.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import wcworkshop.core.data.Wc1GameData;
+import wcworkshop.core.data.Wc1SeriesSlot;
+import wcworkshop.core.reader.Wc1GameDataReader;
+
 @Controller
 public class SeriesEditorController {
+
+  private Wc1GameDataReader gameDataReader = Wc1GameDataReader.getInstance();
 
   @RequestMapping("/seriesEditor.html")
   public String renderEditor(@RequestParam int seriesIndex, Model model) {
 
-    model.addAttribute("series", seriesIndex);
+    Wc1GameData gameData = gameDataReader.readData();
+    List<Wc1SeriesSlot> seriesSlots = gameData.getSeriesSlots();
+    Wc1SeriesSlot series = seriesSlots.get(seriesIndex);
+
+    model.addAttribute("series", series);
 
     return "editors/seriesEditor";
   }
--- wcworkshop-web/src/main/webapp/WEB-INF/jsp/editors/seriesEditor.jsp
+++ wcworkshop-web/src/main/webapp/WEB-INF/jsp/editors/seriesEditor.jsp
@@ -1,1 +1,37 @@
-Series Editor ${series}
+<table>
+  <tbody>
+    <tr>
+      <th>Wingman</th>
+      <td>${series.wingman}</td>
+    </tr>
+    <tr>
+      <th>Nr of Missions</th>
+      <td>${series.nrOfMissions}</td>
+    </tr>
+    <tr>
+      <th>Victory Points</th>
+      <td>${series.victoryPoints}</td>
+    </tr>
+    <tr>
+      <th>Missiontree Level</th>
+      <td>${series.missionTreeLevel}</td>
+    </tr>
+    <tr>
+      <th>Victory Destination</th>
+      <td><a href="javascript:openSeriesEditor(${series.victoryDestination-1})">${series.victoryDestination}</a></td>
+    </tr>
+    <tr>
+      <th>Victory Ship</th>
+      <td>${series.victoryShip}</td>
+    </tr>
+    <tr>
+      <th>Loss Destination</th>
+      <td><a href="javascript:openSeriesEditor(${series.lossDestination-1})">${series.lossDestination}</a></td>
+    </tr>
+    <tr>
+      <th>Loss Ship</th>
+      <td>${series.lossShip}</td>
+    </tr>
+  </tbody>
+
+</table>
