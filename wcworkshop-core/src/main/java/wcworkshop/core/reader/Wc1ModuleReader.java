f3a6673fdd390f0d4865eced007a3e3c1d173d76
delMar43 <martingu1979@gmail.com>
2013-11-28 13:42:00 +0100

    made some progress with offsets

--- wcworkshop-core/src/main/java/wcworkshop/core/reader/Wc1ModuleReader.java
+++ wcworkshop-core/src/main/java/wcworkshop/core/reader/Wc1ModuleReader.java
@@ -24,25 +24,22 @@ public class Wc1ModuleReader {
     List<Integer> blockOffsets = readerHelper.extractBlockOffsets(buffer);
 
     //    System.out.println("Block 1:");
-    extractBlock1(result, Arrays.copyOfRange(buffer, blockOffsets.get(1), blockOffsets.get(2)));
+    //    extractBlock1(result, Arrays.copyOfRange(buffer, blockOffsets.get(1), blockOffsets.get(2)));
 
-    //    System.out.println("Block 2:");
-    extractBlock2(result, Arrays.copyOfRange(buffer, blockOffsets.get(1), blockOffsets.get(2)));
+    System.out.println("Block 2:");
+    extractNavPoints(Arrays.copyOfRange(buffer, blockOffsets.get(1), blockOffsets.get(2)));
 
     //    System.out.println("Block 3:");
-    extractThirdBlock(result, Arrays.copyOfRange(buffer, blockOffsets.get(2), blockOffsets.get(3)), blockOffsets.get(2));
+    //    extractThirdBlock(result, Arrays.copyOfRange(buffer, blockOffsets.get(2), blockOffsets.get(3)), blockOffsets.get(2));
 
     //    System.out.println("Block 4:");
     //    extractSizedBlock(result, Arrays.copyOfRange(buffer, result.getBlockOffset(3), result.getBlockOffset(4)), result.getBlockOffset(3));
 
     //    System.out.println("Block 5:");
-    extractSizedBlock(result, Arrays.copyOfRange(buffer, blockOffsets.get(4), blockOffsets.get(5)), blockOffsets.get(4), 40);
+    //    extractSizedBlock(result, Arrays.copyOfRange(buffer, blockOffsets.get(4), blockOffsets.get(5)), blockOffsets.get(4), 40);
 
     //    System.out.println("Block 6:");
-    extractSizedBlock(result, Arrays.copyOfRange(buffer, blockOffsets.get(5), filesize), blockOffsets.get(5), 40);
-    // extractFirstBlock(result, Arrays.copyOfRange(buffer, result.getBlockOffset(0), result.getBlockOffset(1)));
-    // extractSecondBlock(result, Arrays.copyOfRange(buffer, result.getBlockOffset(1), result.getBlockOffset(2)));
-    // extractThirdBlock(result, Arrays.copyOfRange(buffer, result.getBlockOffset(2), result.getFilesize()));
+    //    extractSizedBlock(result, Arrays.copyOfRange(buffer, blockOffsets.get(5), filesize), blockOffsets.get(5), 40);
 
     return result;
   }
@@ -57,58 +54,75 @@ public class Wc1ModuleReader {
   /**
    * contains nav point data
    */
-  private void extractBlock2(Wc1ModuleData data, byte[] buffer) {
-
+  private List<Wc1NavPoint> extractNavPoints(byte[] buffer) {
     // nav points start at relative 0x1340, chunk length=0x4d (77 dez)
     int offset = 0x1340;
     int chunkSize = 77;
-    int counter = 0;
-    int prevIndex = offset;
-    for (int index = offset; index < buffer.length; index += chunkSize) {
-      Wc1NavPoint navPoint = new Wc1NavPoint();
-      if (buffer[index] == (byte) 0x0) {
-        continue;
-      }
-      String id = new String(Arrays.copyOfRange(buffer, index, index + 30));
-      navPoint.setId(id.substring(0, id.indexOf("\0")));
-      navPoint.setVisible(!id.startsWith("."));
-      navPoint.setUnknown1(readerHelper.getShort(Arrays.copyOfRange(buffer, index + 30, index + 32)));
+    int navsPerMission = 16;
+    int previousIndex = offset;
+    List<Wc1NavPoint> result = new ArrayList<>();
+    //    for (int index = offset; index < buffer.length; index += chunkSize) {
+    int index = offset;
+    for (int series = 0; series < 12; ++series) {
+      for (int mission = 0; mission < 4; ++mission) {
+        for (int nav = 0; nav < navsPerMission; ++nav) {
+          int curOffset = offset + (nav * 77) + (mission * 16 * 77) + (series * 64 * 77);
+          System.out.println("S" + (series + 1) + "M" + (mission + 1) + "N" + (nav + 1) + " - " + curOffset);
 
-      byte[] xBytes = Arrays.copyOfRange(buffer, index + 32, index + 34);
-      navPoint.setxPos(readerHelper.getShort(xBytes));
-      navPoint.setUnknown2(readerHelper.getShort(new byte[] { buffer[index + 34], buffer[index + 35] }));
-      byte[] yBytes = Arrays.copyOfRange(buffer, index + 36, index + 38);
-      navPoint.setyPos(readerHelper.getShort(yBytes));
-      navPoint.setInSystemJumpPoint(buffer[index + 38]);
-      navPoint.setIsJumpPoint(buffer[index + 39]);
-
-      List<Wc1NavPointManipulation> navManList = new ArrayList<>();
-      for (int i = 0; i < 8; i += 2) {
-        byte enableByte = buffer[index + 45 + i];
-        if (enableByte == (byte) 0xff) {
-          break;
+          byte[] curBuffer = Arrays.copyOfRange(buffer, curOffset, curOffset + chunkSize);
+          Wc1NavPoint navPoint = extractNavPointData(curBuffer);
+          result.add(navPoint);
+          System.out.println((index - previousIndex) + " " + index + " 0x" + Integer.toHexString(index).toUpperCase() + " - "
+              + navPoint.toString());
+          previousIndex = index;
+          index += chunkSize;
         }
-        Wc1NavPointManipulation npm = new Wc1NavPointManipulation();
-        npm.setEnableNavPoint(enableByte == 1);
-        npm.setNavPoint(buffer[index + 46 + i]);
-        navManList.add(npm);
+        System.out.println("---");
       }
-      navPoint.setNavPointManipulations(navManList);
-
-      List<Short> shipsToLoad = new ArrayList<>();
-      for (int i = 0; i < 3; i += 2) {
-        short shipToLoad = readerHelper.getShort(new byte[] { buffer[index + i + 53], buffer[index + i + 54] });
-        if (shipToLoad == (short) 0xffff) {
-          break;
-        }
-        shipsToLoad.add(shipToLoad);
-      }
-      navPoint.setShipsToLoad(shipsToLoad);
-
-      System.out.println((index - prevIndex) + " " + index + " 0x" + Integer.toHexString(index).toUpperCase() + " - " + ++counter
-          + navPoint.toString());
-      prevIndex = index;
     }
+    return result;
+  }
+
+  private Wc1NavPoint extractNavPointData(byte[] curBuffer) {
+    Wc1NavPoint navPoint = new Wc1NavPoint();
+
+    String id = new String(Arrays.copyOfRange(curBuffer, 0, 30));
+    navPoint.setId(id.substring(0, id.indexOf("\0")));
+    navPoint.setVisible(!id.startsWith("."));
+    navPoint.setUnknown1(readerHelper.getShort(Arrays.copyOfRange(curBuffer, 30, 32)));
+
+    byte[] xBytes = Arrays.copyOfRange(curBuffer, 32, 34);
+    navPoint.setxPos(readerHelper.getShort(xBytes));
+    navPoint.setUnknown2(readerHelper.getShort(new byte[] { curBuffer[34], curBuffer[35] }));
+    byte[] yBytes = Arrays.copyOfRange(curBuffer, 36, 38);
+    navPoint.setyPos(readerHelper.getShort(yBytes));
+    navPoint.setInSystemJumpPoint(curBuffer[38]);
+    navPoint.setIsJumpPoint(curBuffer[39]);
+
+    List<Wc1NavPointManipulation> navManList = new ArrayList<>();
+    for (int i = 0; i < 8; i += 2) {
+      byte enableByte = curBuffer[45 + i];
+      if (enableByte == (byte) 0xff) {
+        break;
+      }
+      Wc1NavPointManipulation npm = new Wc1NavPointManipulation();
+      npm.setEnableNavPoint(enableByte == 1);
+      npm.setNavPoint(curBuffer[46 + i]);
+      navManList.add(npm);
+    }
+    navPoint.setNavPointManipulations(navManList);
+
+    List<Short> shipsToLoad = new ArrayList<>();
+    for (int i = 0; i < 3; i += 2) {
+      short shipToLoad = readerHelper.getShort(new byte[] { curBuffer[i + 53], curBuffer[i + 54] });
+      if (shipToLoad == (short) 0xffff) {
+        break;
+      }
+      shipsToLoad.add(shipToLoad);
+    }
+    navPoint.setShipsToLoad(shipsToLoad);
+
+    return navPoint;
   }
 
   private void extractThirdBlock(Wc1ModuleData result, byte[] copyOfRange, int blockOffset) {
