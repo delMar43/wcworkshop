f8e0627e00392e1ffd1eac6f1a78d3da2ab9d7f4
delMar43 <martingu1979@gmail.com>
2013-12-04 10:25:49 +0100

    added some unknown bytes

--- binmap/src/test/java/test/Wc1Savegame.java
+++ binmap/src/test/java/test/Wc1Savegame.java
@@ -4,10 +4,10 @@ public class Wc1Savegame {
   public static final Wc1Savegame EMPTY = new Wc1Savegame();
 
   private String name;
-  private short campaign;
+  private byte unknown1;
+  private byte occupied;
   private byte series;
   private byte mission;
-  private byte occupied;
   private Wc1SavegameScoreboardEntry[] scoreboardEntries;
   private short[] pilotStatus;
   private byte bronzeStars;
@@ -18,9 +18,13 @@ public class Wc1Savegame {
   private byte ace2;
   private byte ace3;
   private byte ace4;
-  private byte day;
+  private short day;
+  private short year;
+  private short unknown2;
+  private short unknown3;
   private byte promotion;
   private short victory;
+  private short campaign;
 
   public String getName() {
     return name;
@@ -28,6 +32,14 @@ public class Wc1Savegame {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public byte getUnknown1() {
+    return unknown1;
+  }
+
+  public void setUnknown1(byte unknown1) {
+    this.unknown1 = unknown1;
   }
 
   public short getCampaign() {
@@ -77,4 +89,117 @@ public class Wc1Savegame {
   public void setPilotStatus(short[] pilotStatus) {
     this.pilotStatus = pilotStatus;
   }
+
+  public byte getBronzeStars() {
+    return bronzeStars;
+  }
+
+  public void setBronzeStars(byte bronzeStars) {
+    this.bronzeStars = bronzeStars;
+  }
+
+  public byte getSilverStars() {
+    return silverStars;
+  }
+
+  public void setSilverStars(byte silverStars) {
+    this.silverStars = silverStars;
+  }
+
+  public byte getGoldStars() {
+    return goldStars;
+  }
+
+  public void setGoldStars(byte goldStars) {
+    this.goldStars = goldStars;
+  }
+
+  public byte getGoldenSun() {
+    return goldenSun;
+  }
+
+  public void setGoldenSun(byte goldenSun) {
+    this.goldenSun = goldenSun;
+  }
+
+  public byte getAce1() {
+    return ace1;
+  }
+
+  public void setAce1(byte ace1) {
+    this.ace1 = ace1;
+  }
+
+  public byte getAce2() {
+    return ace2;
+  }
+
+  public void setAce2(byte ace2) {
+    this.ace2 = ace2;
+  }
+
+  public byte getAce3() {
+    return ace3;
+  }
+
+  public void setAce3(byte ace3) {
+    this.ace3 = ace3;
+  }
+
+  public byte getAce4() {
+    return ace4;
+  }
+
+  public void setAce4(byte ace4) {
+    this.ace4 = ace4;
+  }
+
+  public short getDay() {
+    return day;
+  }
+
+  public void setDay(short day) {
+    this.day = day;
+  }
+
+  public short getYear() {
+    return year;
+  }
+
+  public void setYear(short year) {
+    this.year = year;
+  }
+
+  public short getUnknown2() {
+    return unknown2;
+  }
+
+  public void setUnknown2(short unknown2) {
+    this.unknown2 = unknown2;
+  }
+
+  public short getUnknown3() {
+    return unknown3;
+  }
+
+  public void setUnknown3(short unknown3) {
+    this.unknown3 = unknown3;
+  }
+
+  public byte getPromotion() {
+    return promotion;
+  }
+
+  public void setPromotion(byte promotion) {
+    this.promotion = promotion;
+  }
+
+  public short getVictory() {
+    return victory;
+  }
+
+  public void setVictory(short victory) {
+    this.victory = victory;
+  }
+
 }
