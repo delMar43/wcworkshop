7711c611e042f88f6dcdd72ce42780d979a74f1c
delMar43 <martingu1979@gmail.com>
2013-12-09 13:24:47 +0100

    start work on Vxx files

---
+++ wcworkshop-core/src/main/java/wcworkshop/core/binary/Wc1RleBlock.java
@@ -0,0 +1,14 @@
+package wcworkshop.core.binary;
+
+public class Wc1RleBlock {
+
+  private byte[] data;
+
+  public byte[] getData() {
+    return data;
+  }
+
+  public void setData(byte[] data) {
+    this.data = data;
+  }
+}
