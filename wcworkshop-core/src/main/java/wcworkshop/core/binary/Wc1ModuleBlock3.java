b785527187e8eb698214d385c0373f45e30e4a5a
delMar43 <martingu1979@gmail.com>
2013-12-10 20:34:07 +0100

    added all that's known from module block3

---
+++ wcworkshop-core/src/main/java/wcworkshop/core/binary/Wc1ModuleBlock3.java
@@ -0,0 +1,7 @@
+package wcworkshop.core.binary;
+
+public class Wc1ModuleBlock3 {
+
+  private byte[] unknown1;
+  private Wc1ModuleNavPointMapData[] navPointMapData;
+}
--- wcworkshop-core/src/main/java/wcworkshop/core/binary/Wc1ModuleFile.java
+++ wcworkshop-core/src/main/java/wcworkshop/core/binary/Wc1ModuleFile.java
@@ -3,6 +3,7 @@ package wcworkshop.core.binary;
 public class Wc1ModuleFile {
 
   private int filesize;
-  private Wc1ModuleBlock1 block1;
-  private Wc1ModuleBlock2 block2;
+  private Wc1ModuleBlock1 block1; //autopilot
+  private Wc1ModuleBlock2 block2; //navpoints
+  private Wc1ModuleBlock3 block3; //navpoint notes
 }
