9d519e9c31f0c4831d41b1a193d72265f25e0328
delMar43 <martingu1979@gmail.com>
2013-12-13 07:49:21 +0100

    now writing generated file to disk

--- wcworkshop-core/src/test/java/wcworkshop/service/Wc1CutsceneLifecycleServiceTest.java
+++ wcworkshop-core/src/test/java/wcworkshop/service/Wc1CutsceneLifecycleServiceTest.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 import wcworkshop.core.service.Wc1CutsceneReadService;
 import wcworkshop.core.service.Wc1CutsceneWriteService;
 import wcworkshop.core.service.Wc1Cutscenes;
+import wcworkshop.core.writer.WriterHelper;
 
 public class Wc1CutsceneLifecycleServiceTest {
 
   private Wc1CutsceneReadService readService = Wc1CutsceneReadService.getInstance();
   private Wc1CutsceneWriteService writeService = Wc1CutsceneWriteService.getInstance();
+  private WriterHelper writerHelper = WriterHelper.getInstance();
 
   @Test
   public void outputEqualsInput() {
@@ -19,6 +21,9 @@ public class Wc1CutsceneLifecycleServiceTest {
 
     byte[] generated = writeService.generateCutscenes(cutscenes);
 
+    writerHelper.writeFile(generated, "d:/generated.000");
+
     System.out.println("done");
   }
+
 }
