295720530c3a525c1d4824dbe44cf7a4e8a0c411
delMar43 <martingu1979@gmail.com>
2013-12-18 13:26:05 +0100

    added some mapping data for exe files

--- wcworkshop-core/src/main/java/wcworkshop/core/binary/Wc1ExeFile.java
+++ wcworkshop-core/src/main/java/wcworkshop/core/binary/Wc1ExeFile.java
@@ -2,4 +2,23 @@ package wcworkshop.core.binary;
 
 public class Wc1ExeFile {
 
+  private short startDay;
+  private short startYear;
+
+  public short getStartDay() {
+    return startDay;
+  }
+
+  public void setStartDay(short startDay) {
+    this.startDay = startDay;
+  }
+
+  public short getStartYear() {
+    return startYear;
+  }
+
+  public void setStartYear(short startYear) {
+    this.startYear = startYear;
+  }
+
 }
