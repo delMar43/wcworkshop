accbf7d179b2a6b8fc5eb478030c0ce77d3ba25c
delMar43 <martingu1979@gmail.com>
2013-12-27 00:04:11 +0100

    check for already existing username

--- wcworkshop-web/src/main/java/wcworkshop/web/controller/SignupPostController.java
+++ wcworkshop-web/src/main/java/wcworkshop/web/controller/SignupPostController.java
@@ -3,8 +3,10 @@ package wcworkshop.web.controller;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -25,15 +27,34 @@ public class SignupPostController {
   public String receiveRequest(@ModelAttribute SignupCommand signupCommand) {
     logger.info("Signup received");
 
+    File usersFile = new File(Configuration.getInstance().getResourcePath() + "/data/security.properties");
+
     String result;
+
+    try {
+      List<String> lines = FileUtils.readLines(usersFile);
+      for (String line : lines) {
+        if (StringUtils.isBlank(line) || !line.startsWith("user.")) {
+          continue;
+        }
+        line = line.replaceFirst("user.", "");
+        line = line.substring(0, line.indexOf("=") - 1);
+        if (line.equals(signupCommand.getUsername())) {
+          return "redirect:/signup.html";
+        }
+      }
+    } catch (IOException e1) {
+      logger.error("Exception while trying to read registration properties file: {}", e1.getMessage());
+      return "redirect:/signup.html";
+    }
+
     if (!signupCommand.getPassword().equals(signupCommand.getPasswordConfirm())) {
       result = "redirect:/signup.html";
     } else {
 
       String line = "user." + signupCommand.getUsername() + " = " + signupCommand.getPassword() + ", user";
       try {
-        FileUtils.writeLines(new File(Configuration.getInstance().getResourcePath() + "/data/security.properties"),
-            Collections.singletonList(line), true);
+        FileUtils.writeLines(usersFile, Collections.singletonList(line), true);
       } catch (IOException e) {
         logger.error("Exception while trying to write registration to properties file: {}", e.getMessage());
       }
