6c04e5ab1bbf879257925375b3e09da79e965011
delMar43 <martingu1979@gmail.com>
2013-12-28 23:57:34 +0100

    refactoring projectTree

--- wcworkshop-web/src/main/java/wcworkshop/web/controller/ProjectTreeJsonController.java
+++ wcworkshop-web/src/main/java/wcworkshop/web/controller/ProjectTreeJsonController.java
@@ -7,23 +7,39 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import wcworkshop.core.factory.UiFactory;
 import wcworkshop.core.model.tree.ProjectNode;
 
 @Controller
-public class ProjectTreeController {
+public class ProjectTreeJsonController {
   private UiFactory uiFactory = UiFactory.getInstance();
 
-  @RequestMapping("/projectTree.html")
+  @ResponseBody
+  @RequestMapping("/projectTree.json")
   public String renderTree(Model model) {
     Subject subject = SecurityUtils.getSubject();
 
     String username = (String) subject.getPrincipal();
     List<ProjectNode> projectNodes = uiFactory.loadProjectTree(username);
 
-    model.addAttribute("projectNodes", projectNodes);
+    StringBuilder result = new StringBuilder("[");
+    int nrNodes = projectNodes.size();
+    for (int nodeIdx = 0; nodeIdx < nrNodes; ++nodeIdx) {
+      ProjectNode node = projectNodes.get(nodeIdx);
+      result.append(node.toFancyJson());
 
-    return "projectTree";
+      if (!isLastNode(nodeIdx, nrNodes)) {
+        result.append(",");
+      }
+    }
+    result.append("]");
+
+    return result.toString();
+  }
+
+  private boolean isLastNode(int curNode, int nrNodes) {
+    return curNode >= nrNodes - 1;
   }
 }
