c40eed6151b361a245c9cf849a3fb235fa05543f
delMar43 <martingu1979@gmail.com>
2014-01-11 21:45:42 +0100

    working on projectTree

---
+++ wcworkshop-jsf/src/main/java/wellfield/jsf/controller/MainController.java
@@ -0,0 +1,46 @@
+package wellfield.jsf.controller;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import org.primefaces.component.tabview.TabView;
+import org.primefaces.event.NodeSelectEvent;
+import org.primefaces.model.TreeNode;
+
+import wellfield.jsf.factory.ProjectTreeFactory;
+
+@RequestScoped
+@SuppressWarnings("serial")
+@ManagedBean(name = "mainController")
+public class MainController implements Serializable {
+  private ProjectTreeFactory projectTreeFactory = ProjectTreeFactory.getInstance();
+
+  private TabView editorTabs = new TabView();
+  private TreeNode selectedNode;
+
+  public TreeNode getProjects() {
+    return projectTreeFactory.generateProjectTree("delMar");
+  }
+
+  public void onNodeSelect(NodeSelectEvent event) {
+    System.out.println("hih");
+  }
+
+  public TabView getEditorTabs() {
+    return editorTabs;
+  }
+
+  public void setEditorTabs(TabView editorTabs) {
+    this.editorTabs = editorTabs;
+  }
+
+  public TreeNode getSelectedNode() {
+    return selectedNode;
+  }
+
+  public void setSelectedNode(TreeNode selectedNode) {
+    this.selectedNode = selectedNode;
+  }
+}
