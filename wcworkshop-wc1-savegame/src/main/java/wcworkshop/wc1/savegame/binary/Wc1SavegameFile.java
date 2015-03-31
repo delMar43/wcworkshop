1b3c33d1ffa6cd16ada1a9c690bca8685231f7bb
delMar43 <martingu1979@gmail.com>
2015-03-31 23:02:23 +0200

    moved savegame stuff to separate project

---
+++ wcworkshop-wc1-savegame/src/main/java/wcworkshop/wc1/savegame/binary/Wc1SavegameFile.java
@@ -0,0 +1,14 @@
+package wcworkshop.wc1.savegame.binary;
+
+
+public class Wc1SavegameFile {
+  private Wc1Savegame[] savegames;
+
+  public Wc1Savegame[] getSavegames() {
+    return savegames;
+  }
+
+  public void setSavegames(Wc1Savegame[] savegames) {
+    this.savegames = savegames;
+  }
+}
